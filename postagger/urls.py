bed2164faab28e1c2679f511bb81f7b714098dc7
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2024-12-15 18:30:49 +0530

    Delete postagger directory

--- postagger/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('save-sentence/', views.save_sentence, name='save_sentence'),
-    path('save-tags/', views.save_pos_tags, name='save_pos_tags'),
-]
-
